f3a1b334c5e034cf040efc22615cfa03dd0ebd1c
Evgeniy Ionov <evg.ionov@gmail.com>
2024-10-30 09:59:33 +0300

    HomeWork

--- app.py
+++ app.py
@@ -36,15 +36,20 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route("/quotes")
 def get_quotes():
-    select_quotes = "SELECT * from quotes"
+    sql = "SELECT * from quotes;"
     connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    cursor.execute(select_quotes)
-    quotes_db = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        quotes_db = cursor.fetchall()
+    except:
+        return jsonify(error="Some database error"), 500
+    finally:
+        cursor.close()
+        connection.close()
 
     keys = ("id", "author", "text")
+    
     quotes = []
 
     for quote_db in quotes_db:
@@ -55,31 +60,100 @@ def get_quotes():
     
 @app.route("/quotes/<int:quote_id>")
 def get_quote(quote_id):
-    for quote in quotes:
-        if quote["id"] == quote_id:
-            return jsonify(quote), 200
-    return jsonify(error=f"Quote with id={quote_id} not found"), 404
+    sql = "SELECT * from quotes where id = ?;"
+    connection = sqlite3.connect("store.db")
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql, (quote_id, ))
+        quotes_db = cursor.fetchall()
+    except:
+        return jsonify(error="Some database error"), 500
+    finally:
+        cursor.close()
+        connection.close()
 
-@app.get("/quotes/count")
-def quotes_count():
-    return jsonify(count=len(quotes))
+    keys = ("id", "author", "text")
+    
+    quotes = []
+
+    for quote_db in quotes_db:
+        quote = dict(zip(keys, quote_db))
+        quotes.append(quote)
+
+    return jsonify(quotes), 200
     
 @app.route("/quotes", methods=['POST'])
 def create_quote():
-    new_quote = request.json
-    last_quote = quotes[-1]
-    new_id = last_quote["id"] + 1
-    new_quote["id"] = new_id
-    quotes.append(new_quote)
-    return jsonify(new_quote), 201
+    
+    args = request.json
+    author = args.get("author")
+    text = args.get("text")
+
+    if (author == None) or (text == None):
+        return jsonify(error="Input param error"), 400
+    
+    sql = "insert into quotes (author, text) values(?, ?);"
+    connection = sqlite3.connect("store.db")
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql, (author, text))
+        connection.commit()
+    except:
+        return jsonify(error="Some database error"), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+    return jsonify([]), 200
+
+@app.route("/quotes/<int:quote_id>", methods=['PUT'])
+def update_quote(quote_id):
+    
+    args = request.json
+    id = quote_id
+    author = args.get("author")
+    text = args.get("text")
+
+    if (author == None) or (text == None) or (id == None):
+        return jsonify(error="Input param error"), 400
+    
+    sql = "update quotes set author = ?, text = ? where id = ?;"
+    connection = sqlite3.connect("store.db")
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql, (author, text, id))
+        connection.commit()
+    except:
+        return jsonify(error="Some database error"), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+    return jsonify([]), 200
 
 @app.route("/quotes/<int:quote_id>", methods=['DELETE'])
 def delete_quote(quote_id: int):
-    for quote in quotes:
-        if quote["id"] == quote_id:
-            quotes.remove(quote)
-            return jsonify({"message": f"Quote with id={quote_id} has deleted"}), 200
-    return jsonify(error=f"Quote with id={quote_id} not found"), 404
+    id = quote_id
+
+    print(id)
+
+    if (id == None):
+        return jsonify(error="Input param error"), 400
+    
+    sql = "delete from quotes where id = ?;"
+    connection = sqlite3.connect("store.db")
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql, (id, ))
+        connection.commit()
+    except:
+        return jsonify(error="Some database error"), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+    return jsonify([]), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
